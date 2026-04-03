a072dad32e7f14e76c9ef61da8892aa23a31a8df
Felipe Wasaff <fegonzalezw@gmail.com>
2026-04-03 16:02:13 -0300

    Revert "feat(web): rediseño cinematográfico estilo Tarantino"
    This reverts commit 0b25aa4dc627c3eb602f83759098ed299de1f618.

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,18 @@
-import Hero           from '@/components/home/Hero';
-import CasosUso       from '@/components/home/CasosUso';
-import Servicios      from '@/components/home/Servicios';
-import Proyectos      from '@/components/home/Proyectos';
-import Quien          from '@/components/home/Quien';
-import Contacto       from '@/components/home/Contacto';
-import ChapterDivider from '@/components/ChapterDivider';
+import Hero      from '@/components/home/Hero';
+import CasosUso  from '@/components/home/CasosUso';
+import Servicios from '@/components/home/Servicios';
+import Proyectos from '@/components/home/Proyectos';
+import Quien     from '@/components/home/Quien';
+import Contacto  from '@/components/home/Contacto';
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <ChapterDivider num="I"   title="Los Problemas Que Sabemos Resolver" />
       <CasosUso />
-      <ChapterDivider num="II"  title="Capacidades Técnicas" />
       <Servicios />
-      <ChapterDivider num="III" title="Compromisos Ejecutados" />
       <Proyectos />
-      <ChapterDivider num="IV"  title="La Diferencia es el Rigor" />
       <Quien />
-      <ChapterDivider num="V"   title="Iniciar un Proyecto" />
       <Contacto />
     </>
   );
